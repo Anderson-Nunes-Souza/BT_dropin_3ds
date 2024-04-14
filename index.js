d635a86b1bc7ee5e7675a0c5db0a77b26861b522
Anderson Souza <anderson.nunes.souza@hotmail.com>
2024-04-14 02:20:38 -0300

    - Front-end functioning

---
+++ index.js
@@ -0,0 +1,39 @@
+const form = document.getElementById("payment-form");
+
+var authorization = $.ajax({
+  type: "GET",
+  url: "authorization.php",
+  data: [],
+  success: function (result) {
+    console.log(result); // funcionou
+    criarBraintree(result);
+  },
+  error: function (error) {
+    alert(error);
+  },
+});
+
+function criarBraintree(token) {
+  braintree.dropin.create(
+    {
+      authorization: token,
+      container: "#dropin-container",
+    },
+    (error, dropinInstance) => {
+      if (error) console.error(error);
+      form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        dropinInstance.requestPaymentMethod((error, payload) => {
+          if (error) console.error(error);
+          // Step four: when the user is ready to complete their
+          //   transaction, use the dropinInstance to get a payment
+          //   method nonce for the user's selected payment method, then add
+          //   it a the hidden field before submitting the complete form to
+          //   a server-side integration
+          document.getElementById("nonce").value = payload.nonce;
+          form.submit();
+        });
+      });
+    }
+  );
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '241547c1ff211c387f3c3bb3087a87eb691a3e6c',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '241547c1ff211c387f3c3bb3087a87eb691a3e6c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'braintree/braintree_php' => array(
+            'pretty_version' => '6.17.0',
+            'version' => '6.17.0.0',
+            'reference' => '37c187c91416003708632a58c230d03dbe88fb67',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../braintree/braintree_php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
